8d30ef1709a8c19fc51923fb229666c0b2cebf7b
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-23 22:56:23 +0530

    adding google auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screen/login_screen.dart';
+import 'package:reddit_clone/firebase_options.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reddit clone',
@@ -19,4 +23,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
