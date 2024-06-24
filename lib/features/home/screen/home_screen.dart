8d52bd1d6ad2a69706c007384c84ffd75a35cace
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-24 20:38:17 +0530

    adding home scree

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/home/drawers/community_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () => displayDrawer(context),
+            icon: const Icon(Icons.menu),
+          );
+        }),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePicture),
+            ),
+          )
+        ],
+      ),
+      drawer: const CommunityListDrawer(),
       body: Center(
         child: Text(user.name),
       ),
