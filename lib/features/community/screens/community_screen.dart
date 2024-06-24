826d378fa4cd818b6358a17d7385e6ff7f77919a
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-24 20:51:15 +0530

    adding create community screen

---
+++ lib/features/community/screens/community_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CreateCommunityScreen extends ConsumerStatefulWidget {
+  const CreateCommunityScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CreateCommunityScreenState();
+}
+
+class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -2,21 +2,18 @@
 
 //logged in route
 
-
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screen/login_screen.dart';
+import 'package:reddit_clone/features/community/screens/community_screen.dart';
 import 'package:reddit_clone/features/home/screen/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
-final loggedOutRoute = RouteMap(
-  routes: {
-    '/': (_) => const MaterialPage(child: LoginScreen()),
-  }
-);
+final loggedOutRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: LoginScreen()),
+});
 
-final loggedInRoute = RouteMap(
-  routes: {
-    '/': (_) => const MaterialPage(child: HomeScreen()),
-  }
-);
-
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) =>
+      const MaterialPage(child: CreateCommunityScreen()),
+});
