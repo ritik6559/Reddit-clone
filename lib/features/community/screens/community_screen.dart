b2e3b965db65c970f552b553e97231941126539e
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-24 21:05:56 +0530

    community screen ui completed

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -5,22 +5,62 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CreateCommunityScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _CreateCommunityScreenState();
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
-
+  final TextEditingController communityNameController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    super.dispose();
+    communityNameController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("hello"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create a community"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Community name"),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                filled: true,
+                hintText: "r/community_name",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(double.infinity, 50)),
+              onPressed: () {},
+              child: Text(
+                "Create community",
+                style: TextStyle(
+                  fontSize: 17,
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
