b8c1a3fe68d76ec972f8fec2aec9f2fa0deef5e4
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-26 13:37:02 +0530

    completing community details screen ui

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/components/error_txt.dart';
 import 'package:reddit_clone/components/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/community/controller/community_controller.dart';
 
 class CommunityScreen extends ConsumerWidget {
@@ -9,6 +11,7 @@ class CommunityScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider)!;
     return Scaffold(
       body: ref.watch(getCommunityByNameProvider(name)).when(
           data: (community) => NestedScrollView(
@@ -29,11 +32,66 @@ class CommunityScreen extends ConsumerWidget {
                       ],
                     ),
                   ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(16),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(community.avatar),
+                              radius: 35,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "r/${community.name}",
+                                style: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              community.mods.contains(user.uid) ? 
+                              OutlinedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 25),
+                                ),
+                                child: const Text("Mod Tools"),
+                              ) : OutlinedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 25),
+                                ),
+                                child:  Text(community.members.contains(user.uid) ?  "Joined" : "Join"),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text("${community.members.length}"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ];
               },
               body: Container()),
           error: (error, stackTrace) {
-            return;
+            return ErrorText(error: error.toString());
           },
           loading: () => const Loader()),
     );
