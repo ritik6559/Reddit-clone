1cc4084e5d73fa96a1bffc6c93a10ac8555bd34f
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-29 18:44:24 +0530

    adding add post screen

--- lib/features/posts/screen/add_post_type_screen.dart
+++ lib/features/posts/screen/add_post_type_screen.dart
@@ -1,12 +1,20 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/components/error_txt.dart';
+import 'package:reddit_clone/components/loader.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/coummunity_model.dart';
+import 'package:reddit_clone/utils/pick_image.dart';
 
 class AddPostTypeScreen extends ConsumerStatefulWidget {
   final String type;
   const AddPostTypeScreen({
     super.key,
     required this.type,
-    });
+  });
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -14,10 +22,147 @@ class AddPostTypeScreen extends ConsumerStatefulWidget {
 }
 
 class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final linkController = TextEditingController();
+  List<Community> communities = [];
+  File? bannerFile;
+  Community? selectedCommuntiy;
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    descriptionController.dispose();
+    linkController.dispose();
+  }
+
+  void selecteBannerImage() async {
+    final res = await pickImage();
+    if (res != null) {
+      setState(() {
+        bannerFile = File(res.files.first.path!);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isTypeImage = widget.type == 'image';
+    final isTypeText = widget.type == 'text';
+    final isTypeLink = widget.type == 'link';
+
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("Post ${widget.type}"),
+        centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text("Share"),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(
+                filled: true,
+                hintText: "Enter title here",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 30,
+            ),
+            const SizedBox(height: 10),
+            if (isTypeImage)
+              GestureDetector(
+                onTap: selecteBannerImage,
+                child: DottedBorder(
+                  dashPattern: const [10, 4],
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: bannerFile != null
+                        ? Image.file(bannerFile!)
+                        : const Center(
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              size: 40,
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+            if (isTypeText)
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: "Enter description here",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+                maxLines: 5,
+              ),
+            if (isTypeLink)
+              TextField(
+                controller: linkController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: "Enter link here",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+              ),
+            const SizedBox(height: 20),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Select Community",
+              ),
+            ),
+            ref.watch(userCommunitiesProvider).when(
+                  data: (data) {
+                    communities = data;
+                    if (data.isEmpty) {
+                      return const SizedBox(
+                        child: Text("Join community"),
+                      );
+                    }
+                    return DropdownButton(
+                      value: selectedCommuntiy ?? data[0],
+                      items: data
+                          .map(
+                            (e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(e.name),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (val) {
+                        setState(() {
+                          selectedCommuntiy = val;
+                        });
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) =>
+                      ErrorText(error: error.toString()),
+                  loading: () => const Loader(),
+                ),
+          ],
+        ),
+      ),
     );
   }
 }
