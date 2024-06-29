63c936aaeaf55c5d7598a826f78d5e52be9295e1
Ritik6559 <ritikjoshi741@gmail.com>
2024-06-29 20:00:46 +0530

    adding post model

---
+++ lib/features/posts/controller/post_controller.dart
@@ -0,0 +1,154 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/components/snackbar.dart';
+import 'package:reddit_clone/core/providers/storage_repository.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/posts/repository/post_repository.dart';
+import 'package:reddit_clone/models/coummunity_model.dart';
+import 'package:reddit_clone/models/post_model.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:uuid/uuid.dart';
+
+final postControllerProvider =
+    StateNotifierProvider<PostController, bool>((ref) {
+  return PostController(
+      postRepository: ref.watch(postRepositoryProvider),
+      ref: ref,
+      storageRepository: ref.watch(storageRepositoryProvider));
+});
+
+class PostController extends StateNotifier<bool> {
+  final PostRepository _postRepository;
+  final Ref _ref;
+  final StorageRepository _storageRepository;
+
+  PostController({
+    required PostRepository postRepository,
+    required Ref ref,
+    required StorageRepository storageRepository,
+  })  : _postRepository = postRepository,
+        _ref = ref,
+        _storageRepository = storageRepository,
+        super(false);
+
+  void sharedTextPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String description,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+
+    final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'text',
+      createdAt: DateTime.now(),
+      awards: [],
+      description: description,
+    );
+
+    final res = await _postRepository.addPosts(post);
+    state = false;
+
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, "Posted succcessfully!");
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  void sharedLinkPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String link,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+
+    final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'link',
+      createdAt: DateTime.now(),
+      awards: [],
+      link: link,
+    );
+
+    final res = await _postRepository.addPosts(post);
+    state = false;
+
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, "Posted succcessfully!");
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  void sharedFilePost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required File? file,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+    final imageRes = await _storageRepository.storeFile(
+        path: 'posts/${selectedCommunity.name}', id: postId, file: file);
+
+    imageRes.fold((l) => showSnackBar(context, l.message), (r) async{//we will proceed only if we can upload the image.
+      final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'text',
+      createdAt: DateTime.now(),
+      awards: [],
+      link: r,
+    );
+
+    final res = await _postRepository.addPosts(post);
+    state = false;
+
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, "Posted succcessfully!");
+        Routemaster.of(context).pop();
+      },
+    );
+    });
+    
+  }
+}
---
+++ lib/features/posts/repository/post_repository.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/failure.dart';
+import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/core/type_defs.dart';
+import 'package:reddit_clone/models/post_model.dart';
+
+final postRepositoryProvider = Provider((ref) {
+  final firestore = ref.watch(firestoreProvider);
+  return PostRepository(firestore: firestore);
+});
+
+class PostRepository {
+  final FirebaseFirestore _firestore;
+  PostRepository({
+    required FirebaseFirestore firestore,
+  }) : _firestore = firestore;
+
+  CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
+
+  FutureVoid addPosts(Post post) async {
+    try {
+      return right(_posts.doc(post.id).set(post.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
